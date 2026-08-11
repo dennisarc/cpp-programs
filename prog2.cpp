38c7e0dc7a3f0b57be0be16e4e375121d8b77257
APPLE <apple@Denniss-MacBook-Air.local>
2026-08-12 00:09:20 +0530

    Add cpp practice programs

---
+++ prog2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b,sub;
+    cout<<"enter first number"<<endl;
+    cin>>a;
+
+    cout<<"enter second number"<<endl;
+    cin>>b;
+
+    sub=a-b;
+    cout<<sub<<endl;
+    return 0;
+}
---
+++ prog3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int l,b,area;
+    cout<<"enter the length of the rectangle"<<endl;
+    cin>>l;
+
+    cout<<"enter the breadth of the rectangle"<<endl;
+    cin>>b;
+
+    area=l*b;
+    cout<<area<<endl;
+    return 0;
+}
